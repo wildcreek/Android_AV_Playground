132ece7932bf19e3b49601b67d5dd45727a44a91
胡志成 <wildcreek@163.com>
2017-12-07 20:11:02 +0800

    add task3 ，capture camera

--- app/src/main/java/com/wildcreek/playground/MainActivity.java
+++ app/src/main/java/com/wildcreek/playground/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.wildcreek.playground.audio.AudioRecordActivity;
+import com.wildcreek.playground.video.CaptureCameraActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this , AudioRecordActivity.class);
+        Intent intent = new Intent(this , CaptureCameraActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/wildcreek/playground/video/CaptureCameraActivity.java
@@ -0,0 +1,140 @@
+package com.wildcreek.playground.video;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.OrientationEventListener;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.wildcreek.playground.R;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CaptureCameraActivity extends AppCompatActivity {
+
+    private ExecutorService threadPool  = Executors.newSingleThreadExecutor();
+    private SurfaceView surfaceView ;
+    private SurfaceHolder surfaceHolder;
+    private Camera camera;
+    private OrientationEventListener mOrientationListener;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture_camera);
+//        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//        sensorManager.registerListener(new SensorEventListener() {
+//            @Override
+//            public void onSensorChanged(SensorEvent event) {
+//
+//            }
+//
+//            @Override
+//            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//
+//            }
+//        },sensor,SensorManager.SENSOR_DELAY_UI);
+        surfaceView = findViewById(R.id.sv_act_capture_camera);
+        surfaceHolder = surfaceView.getHolder();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        threadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                camera = Camera.open();
+                final Camera.Parameters parameters = camera.getParameters();
+                //parameters.setPreviewSize(640 ,480);
+                //设置相对于摄像头视角的顺时针旋转角度，该方法影响Camera.pictureCallback的图像方向
+                //OrientationEventListener监听正常手机角度变化，CameraInfo.orientation 是与正常状态手机角度差值，
+                //为达到预览实景的效果，后置摄像头需要设置成两者之和，前置摄像头需要设置成两者之差
+                //假设手机顺时针旋转270度，假设后置摄像头的上方为手机右侧，即camera的orientation为90度，所以应该设成两者之和360度
+                mOrientationListener = new OrientationEventListener(CaptureCameraActivity.this , SensorManager.SENSOR_DELAY_NORMAL) {
+                    @Override
+                    public void onOrientationChanged(int orientation) {
+                        Log.e("" ,"onOrientationChanged = " + orientation);
+                        if (orientation == ORIENTATION_UNKNOWN) return;
+                        Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
+                        android.hardware.Camera.getCameraInfo(1, info);
+                        orientation = (orientation + 45) / 90 * 90;
+                        int rotation = 0;
+                        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                            rotation = (info.orientation - orientation + 360) % 360;
+                        } else {  // back-facing camera
+                            rotation = (info.orientation + orientation) % 360;
+                        }
+                        parameters.setRotation(rotation);
+                    }
+                };
+                if (mOrientationListener.canDetectOrientation()) {
+                    Log.e("" ,"onOrientationChanged can DetectOrientation" );
+                    mOrientationListener.enable();
+                } else {
+                    Log.e("" ,"onOrientationChanged can not DetectOrientation" );
+                    mOrientationListener.disable();
+                }
+                //parameters.setRotation(0);
+                camera.setParameters(parameters);
+                //设置预览的顺时针旋转角度，该方法影响预览的图像和拍照后图片的展示方向，但不影响 PreviewCallback的 onPreviewFrame的二进制数据序列
+                //camera.setDisplayOrientation(0);
+                setCameraDisplayOrientation(CaptureCameraActivity.this , 1,camera);
+                try {
+                    camera.setPreviewDisplay(surfaceHolder);
+                    camera.startPreview();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(camera != null){
+            camera.stopPreview();
+            camera.release();
+        }
+    }
+    public static void setCameraDisplayOrientation(Activity activity,int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        //屏幕内容的旋转方向：手机逆时针旋转90，作为补偿，该方法返回Surface.ROTATION_90
+        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mOrientationListener.disable();
+    }
+}
